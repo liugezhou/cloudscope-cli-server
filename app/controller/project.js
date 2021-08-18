f3a0ee7c114ea544d9facd9a2f4ec844b50680e9
liumingzhou <18231133236@163.com>
2021-08-18 21:34:40 +0800

    add:project/oss

--- app/controller/project.js
+++ app/controller/project.js
@@ -2,17 +2,46 @@
 
 const Controller = require('egg').Controller;
 const mongo = require('../utils/mongo');
+const OSS = require('../models/OSS')
+const config = require('../../config/db')
+const {success, failed } = require('../utils/request') 
 class ProjectController extends Controller {
   async getTemplate() {
-    const { ctx, app } = this;
+    const { ctx } = this;
     const data = await mongo().query('project');
     ctx.body = data;
   }
 
+  async getOSSProject(){
+    const { ctx } = this
+    let ossProjectType = ctx.query.type
+    let ossProjectName = ctx.query.name 
+    if(!ossProjectName){
+      ctx.body=failed('项目名称不存在')
+      return
+    }
+    if(ossProjectType){
+      ossProjectType = 'prod'
+    }
+    let oss 
+    if(Object.is(ossProjectType,'prod')){
+       oss = new OSS(config.OSS_DEV_BUCKET)
+    }else{
+      oss = new OSS(config.OSS_PROD_BUCKET)
+    }
+    const ossList = await oss.list(ossProjectName)
+    ctx.body = success('项目文件获取成功',ossList)
+  }
+
   async getRedis(){
     const { ctx, app } = this;
-    const num = await app.redis.get('number')
-    ctx.body = 'hello redis'
+    const { key } = ctx.query
+    if(key){
+      const value = await app.redis.get(key)
+      ctx.body=`redis[${key}]:${value}`
+    }else{
+      ctx.body = '没有获取到key'
+    }
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -6,13 +6,8 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/project/template', controller.project.getTemplate);
+  router.get('/project/oss', controller.project.getOSSProject);
   router.get('/redis/test', controller.project.getRedis);
-
-  // app.io.of('/')
-  // app.io.route('build', app.io.controller.chat.index);
-  app.io.route('build', app.io.controller.build.index);
-
-  // app.io.of('/chat')
-  // app.io.of('/chat').route('chat', app.io.controller.chat.index);
   
+  app.io.route('build', app.io.controller.build.index);
 };
